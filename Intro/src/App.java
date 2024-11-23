8d95cc5de8b8e68266881a2f38e78197aef7df4e
FelipeAlvarado01 <afvalvarado@gmail.com>
2024-11-22 21:04:30 -0500

    Tipos de datos en Java

--- Intro/src/App.java
+++ Intro/src/App.java
@@ -1,5 +1,37 @@
+import javax.swing.SpringLayout;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        String name = "Aprendiendo de cadenas en Java";
+        System.out.println(name);
+        
+        Integer n  = 7; 
+
+        System.out.println("mi integer: "+ n);
+
+        Double nD  = 8.5;
+        System.out.println("Mi Double: " + nD);
+
+        float nF = 6.5f;
+        System.out.println("Mi float: " + nF);
+        
+        boolean myBool = false;
+        if(myBool == true){
+            System.out.println("myBool es True");
+        }else{
+            System.out.println("myBool es False");
+        }
+
+        String nullString = "Hola";
+        nullString= null;
+        
+        if(nullString != null){
+            System.out.println("Es distinto de nulo");
+        }
+        else{
+            System.out.println("Es nulo");
+        }
     }
 }
